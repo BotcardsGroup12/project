38200fa757629e041a88c7a17a167241f301946d
a00909032 <a00909032@gmail.com>
2016-02-14 00:11:40 -0800

    ModelCreation v1

---
+++ application/models/Series.php
@@ -0,0 +1,48 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Class Series extends CI_Model {
+    
+    function getDatetime() {
+        return $this->_datetime;
+    }
+
+    function getPlayer() {
+        return $this->_player;
+    }
+
+    function getSeries() {
+        return $this->_series;
+    }
+
+    function getTrans() {
+        return $this->_trans;
+    }
+
+    function setDatetime($datetime) {
+        $this->_datetime = $datetime;
+    }
+
+    function setPlayer($player) {
+        $this->_player = $player;
+    }
+
+    function setSeries($series) {
+        $this->_series = $series;
+    }
+
+    function setTrans($trans) {
+        $this->_trans = $trans;
+    }
+
+        
+    function __construct() {
+        parent::__construct();
+    }
+}
+
---
+++ application/models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Class User extends CI_Model {
+    function getUsername() {
+        return $this->_username;
+    }
+
+    function getUserpassword() {
+        return $this->_userpassword;
+    }
+
+    function getUsercash() {
+        return $this->_usercash;
+    }
+
+    function setUsername($username) {
+        $this->_username = $username;
+    }
+
+    function setUserpassword($userpassword) {
+        $this->_userpassword = $userpassword;
+    }
+
+    function setUsercash($usercash) {
+        $this->_usercash = $usercash;
+    }
+
+        
+    function __construct() {
+        parent::__construct();
+    }
+}
+
